81adbea58c610062beabec0268b746911fe29c75
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-23 17:29:06 +0530

    done

--- Funfit-Project/Funfit-Project/src/main/java/com/funfit/controller/StudentStoreController.java
+++ Funfit-Project/Funfit-Project/src/main/java/com/funfit/controller/StudentStoreController.java
@@ -1,6 +1,8 @@
 package com.funfit.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,8 +36,15 @@ public class StudentStoreController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter pw=response.getWriter();
+		// receive the value from form and convert to respective data types. 
+		
+		int sid=Integer.parseInt(request.getParameter("sid"));
+		String sname=request.getParameter("sname");
+		int age=Integer.parseInt(request.getParameter("age"));
+		String phone=request.getParameter("phone");
+		String email=request.getParameter("email");
+		int bid=Integer.parseInt(request.getParameter("bid"));
 	}
 
 }
--- Funfit-Project/Funfit-Project/src/main/java/com/funfit/service/StudentService.java
+++ Funfit-Project/Funfit-Project/src/main/java/com/funfit/service/StudentService.java
@@ -7,6 +7,7 @@ public class StudentService {
 	
 	StudentDao sd=new StudentDao();
 	public String storeStuent(Student student) {
+		
 		return null;
 	}
 
